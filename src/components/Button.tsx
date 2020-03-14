1c95f1ad8ea906d13103ed6afee76b357688a200
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-14 15:26:07 +0800

    feat: Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  color: white;
+  width: 130px;
+  height: 40px;
+  border: none;
+  cursor: pointer;
+  border-radius: 2px;
+  background-color: var(--primary-color);
+`;
