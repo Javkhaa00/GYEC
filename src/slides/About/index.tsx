377d2a04e7e5f7e6c6a2222c0d87ee845e08641e
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-14 17:28:57 +0800

    fix: Render members' pictures properly

--- src/slides/About/index.tsx
+++ src/slides/About/index.tsx
@@ -1,27 +1,42 @@
 import React from 'react';
-import Slide from '@/components/Slide';
 import styled from 'styled-components';
 
+import { groupToPairs } from '@/util';
+
+import Slide from '@/components/Slide';
+
 const Container = styled(Slide)`
+  color: white;
   background-color: var(--primary-color);
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  grid-template-areas: 'grid grid grid grid' 'grid grid grid grid';
 `;
 
 const MemberList = styled.div`
-  display: grid;
-  align-items: center;
-  justify-items: center;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 2fr 2fr;
-  grid-area: grid;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
 `;
 const MemberPicture = styled.img`
   --picture-size: 200px;
   height: var(--picture-size);
   width: var(--picture-size);
+  margin: 10px;
+`;
+
+const Title = styled.h1`
+  margin: 10px 0;
+  font-size: 2rem;
+  font-weight: 900;
+`;
+const Description = styled.h3`
+  width: 20rem;
+  font-size: 1rem;
+  text-align: center;
+  font-weight: 700;
+`;
+
+const Col = styled.div`
+  display: flex;
+  flex-flow: column wrap;
 `;
 
 const teamMembers = [
@@ -37,9 +52,18 @@ const teamMembers = [
 function About() {
   return (
     <Container>
+      <Title>About us</Title>
+      <Description>
+        We are a bunch mixed high school students from different schools who are
+        trying to decrease human injuries and increase work power of people.
+      </Description>
       <MemberList>
-        {teamMembers.map(member => (
-          <MemberPicture src={`/assets/members/${member}.jpg`} />
+        {groupToPairs(
+          teamMembers.map(member => (
+            <MemberPicture key={member} src={`/assets/members/${member}.jpg`} />
+          )),
+        ).map((children, i) => (
+          <Col key={i.toString()}>{children}</Col>
         ))}
       </MemberList>
     </Container>
