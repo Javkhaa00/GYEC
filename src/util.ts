bde46d48f788bcaa0e1514348e1ac0e3a4350412
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-14 18:58:07 +0800

    perf: Remove unused code

--- src/util.ts
+++
@@ -1,8 +0,0 @@
-// groupToPairs takes elements of array and returns pairs of these elements
-export function groupToPairs<T>(arr: Array<T>): Array<[T, T]> {
-  const result: Array<[T, T]> = [];
-  for (let i = 0; i < arr.length / 2; i += 1) {
-    result.push([arr[2 * i], arr[2 * i + 1]]);
-  }
-  return result;
-}
