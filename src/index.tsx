204170d8963714e9f0e1b8a5361a3e9abecffa23
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-14 14:11:36 +0800

    perf: Optimize @nicepack/react/indicator usage
    Include and use @nicepack/react/indicator only in non-production build
    by using dynamic import

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import useFastReloadIndicator from '@nicepack/react/indicator';
-
 import GlobalStyles from './components/GlobalStyles';
 import Home from './slides/Home';
+import Product from './slides/Product';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <Home />
+      <Product />
     </>
   );
 }
 
-useFastReloadIndicator();
+if (process.env.NODE_ENV !== 'production') {
+  import('@nicepack/react/indicator').then(
+    ({ default: useIndicator }) =>
+      typeof useIndicator === 'function' && useIndicator(),
+  );
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
