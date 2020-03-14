b331db5a55f3bf4c2900ed79f08ca3a760096948
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-14 16:26:00 +0800

    feat: Add contact us section

---
+++ src/slides/ContactUs.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import Slide from '@/components/Slide';
+
+const Container = styled(Slide)`
+  justify-content: center;
+`;
+const Title = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 800;
+
+  @media screen and (max-width: 500px) {
+    font-size: 1.5rem;
+  }
+`;
+
+const ContactContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 20px;
+  font-size: 1.7rem;
+  > span {
+    margin-right: 15px;
+  }
+  @media screen and (max-width: 500px) {
+    font-size: 0.9rem;
+    > span {
+      margin-right: 5px;
+    }
+  }
+`;
+
+const Contact: React.FC<{
+  name?: string;
+  children: React.ReactNode;
+}> = ({ name, children }) => (
+  <ContactContainer>
+    {name && <span>{`${name}:`}</span>}
+    {children}
+  </ContactContainer>
+);
+Contact.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  name: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+function ContactUs() {
+  return (
+    <Container>
+      <Title>Contact us</Title>
+      <Contact name="Phone number">
+        <a href="tel:+97699135563">+976 99135563</a>
+      </Contact>
+      <Contact name="Email">
+        <a href="mailto:ezenbaatar.batjargal@gmail.com">
+          ezenbaatar.batjargal@gmail.com
+        </a>
+      </Contact>
+      <Contact>UNESCO street, Ulaanbaatar, Mongolia 13000</Contact>
+    </Container>
+  );
+}
+
+export default ContactUs;
