7a9aab28d34c59031f0687428d8e6fe04d9103d1
Javkhaa00 <liplicker0423@gmail.com>
2020-03-14 13:01:08 +0800

    feat(About page add): Page add on index

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import About from "./components/About/index";
 import './index.css';
 import useFastReloadIndicator from '@nicepack/react/indicator';
 
 function App() {
   return (
     <>
+      <About />
       <h2>HI MY PREEND</h2>
       <p>Hello, world!</p>
     </>
