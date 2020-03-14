dac7fa3714d7e8bbb6f2a14729f9bef280dc521f
Javkhaa00 <liplicker0423@gmail.com>
2020-03-14 14:08:25 +0800

    feat(Team profile): Grid, 4x2 profiles

--- src/slides/About/index.tsx
+++ src/slides/About/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Wrapper = styled.section`
   height: 100vh;
   width: 100vw;
-  display: flex;
-  background-color: #19ACBE;
-  // align-items: center;
+  background-color: #19acbe;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  grid-template-areas: 'grid grid grid grid' 'grid grid grid grid';
 `;
 const assets = [
   '/assets/members/ezka.jpg',
@@ -18,20 +20,28 @@ const assets = [
   '/assets/members/gulnvr.jpg',
   '/assets/members/kongro.jpg',
 ];
-const Pictures = styled.div`
-  height: 100px;
-  width: 60px;
+const Grid = styled.div`
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 2fr 2fr;
+  grid-area: grid;
+`;
+const Pictures = styled.img`
+  height: 45%;
+  width: 80%;
 `;
 
 function About() {
   return (
     <>
       <Wrapper>
-        <Pictures>
+        <Grid>
           {assets.map(member => (
-            <img src={member} />
+            <Pictures src={member} />
           ))}
-        </Pictures>
+        </Grid>
       </Wrapper>
     </>
   );
