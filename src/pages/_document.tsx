bd7ba4c17016fd63a7a5914292e06a70d77da1e0
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-15 14:16:57 +0800

    refactor: Remove ambigious meta viewport tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,10 +36,6 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
           <meta name="description" content="Slocklance product presentation." />
           <meta name="robots" content="index, follow" />
         </Head>
