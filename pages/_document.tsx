25f1c6f0953e7ed769dc235d6e679329ad1a1799
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-15 13:44:47 +0800

    feat: Move Head tag and add 'lang' prop for html tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
-import Document from 'next/document';
+import React from 'react';
+
 import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -27,5 +29,26 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="description" content="Slocklance product presentation." />
+          <meta name="robots" content="index, follow" />
+          <title>Nani Tech</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head';
 
 import GlobalStyles from '@/components/GlobalStyles';
 import Home from '@/slides/Home';
@@ -10,13 +9,6 @@ import ContactUs from '@/slides/ContactUs';
 function App() {
   return (
     <>
-      <Head>
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Slocklance product presentation." />
-        <meta name="robots" content="index, follow" />
-        <title>Nani Tech</title>
-      </Head>
       <GlobalStyles />
       <Home />
       <Product />
