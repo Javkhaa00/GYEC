f95afe9c00e3c0154d082f0c7bf00418118c53e3
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-15 19:42:24 +0800

    feat: Remove logo in NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,15 +2,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Logo from '@/components/Logo';
-
 const Container = styled.div`
   width: 100%;
   height: 80px;
   padding: 0 40px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: flex-end;
   @media screen and (max-width: 550px) {
     height: 60px;
     padding: 0 15px;
@@ -35,7 +33,6 @@ const Navigation = styled.nav`
 function NavBar() {
   return (
     <Container>
-      <Logo />
       <Navigation>
         <a href="#home">Home</a>
         <a href="#product">Product</a>
